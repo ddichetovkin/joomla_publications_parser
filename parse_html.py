ce6e2529c77b9243f6b453981dc5deb24e447a96
Danil <ddichetovkin@gmail.com>
2026-01-17 21:58:50 +0300

    add finding image path

--- parse_html.py
+++ parse_html.py
@@ -4,6 +4,7 @@ from markdownify import markdownify as md
 from datetime import datetime
 
 source_dir = "publications"
+target_dir = "content"
 
 class Article:
     def __init__(self ,path: str):
@@ -13,10 +14,10 @@ class Article:
         self.date = datetime.strftime(datetime.strptime(self.header.date.text.strip(), "%d.%m.%Y"), "%Y-%m-%d")
         self.author = self.header.find("div", class_="article-item-credits_author divider").a.text.strip()
         self.title = self.header.h1.text
-        self.description = self.title
         self.text_block = self._get_article_text_block()
         self.new_path_name = self._get_new_path_name()
         self.tag_list = self._get_tag_list()
+        self.image_path = self.soup.find("div", class_="article-item-image").img["src"]
 
     def _get_tag_list(self):
         return self.soup.article.footer.find("div", class_="article-item-tags").text.strip("\n").split("\n")
@@ -29,10 +30,6 @@ class Article:
             html_str = file.read()
         soup = BeautifulSoup(html_str, "lxml")
         return soup
-
-    def get_paragraph_list(self):
-        item_list = self.article.find_all(True, recursive=False)
-        return item_list
 
     def _get_article_text_block(self):
         return self.soup.article.find("div", class_="article-item-text")
